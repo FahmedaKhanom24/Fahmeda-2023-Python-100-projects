b774d545f06ad4af278375f34049c76bcebdd6be
FahmedaKhanom24 <70119503+FahmedaKhanom24@users.noreply.github.com>
2023-02-23 21:56:08 -0500

    PROJECT 3 TREASURE ISLAND

---
+++ PROJECT 3 TREASURE ISLAND.py
@@ -0,0 +1,46 @@
+print('''
+ ,d                                                                       
+  88                                                                       
+MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,  
+  88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8  
+  88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88          
+  88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88          
+  "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          
+                                                                           
+                                                                           
+                                                               
+           88           88                                 88  
+           ""           88                                 88  
+                        88                                 88  
+ ,adPPYba, 88 ,adPPYba, 88 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  
+a8P_____88 88 I8[    "" 88 ""     `Y8 88P'   `"8a a8"    `Y88  
+8PP""""""" 88  `"Y8ba,  88 ,adPPPPP88 88       88 8b       88  
+"8b,   ,aa 88 aa    ]8I 88 88,    ,88 88       88 "8a,   ,d88  
+ `"Ybbd8"' 88 `"YbbdP"' 88 `"8bbdP"Y8 88       88  `"8bbdP"Y8  
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+userinput = input("You are in a park, which direction you want to go? Tyep 'L' for left and 'R' for right ").upper()
+if userinput == 'L':
+    userinput2= input("Do you want to swim('S') or wait('W')?").upper()
+    if userinput2 == 'W':
+        userinput3=input("Which door Red('R'), Blue('B'), Yellow('Y') or anything?").upper()
+        if userinput3=='R':
+            print("Burned by fire. GAME OVER")
+        elif userinput3 == 'B':
+            print("Eaten by Beasts. GAME OVER")
+        elif userinput3 == 'Y':
+            print("You Win")
+        else:
+            print("Game OVER")
+    else:
+        print("Attacked by a trout. GAME OVER")
+
+else:
+    print("Oh, you fall into a hole. GAME OVER")
+
+
